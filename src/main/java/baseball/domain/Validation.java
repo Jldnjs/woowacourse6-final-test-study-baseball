72c20ae97aa621385f4eaeabcae589142de0fce2
Jiwon <dtbsmf01@gmail.com>
2023-11-29 16:47:01 +0900

    style: '

--- src/main/java/baseball/domain/Validation.java
+++ src/main/java/baseball/domain/Validation.java
@@ -10,21 +10,18 @@ public class Validation {
     }
     private static void countValidation(List<Integer> playerNum){
         if (playerNum.size() != 3){
-            System.out.println("[ERROR] 3개의 숫자를 입력하세요.");
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 3개의 숫자를 입력하세요.");
         }
     }
     private static void duplicateValidation(List<Integer> playerNum){
         if (playerNum.size() != playerNum.stream().distinct().count()){
-            System.out.println("[ERROR] 중복된 요소가 있습니다.");
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 중복된 요소가 있습니다.");
         }
     }
 
     public static void restartNumValidation(String num) {
         if (!(num == "1" || num == "2")) {
-            System.out.println("[ERROR] 1또는 2를 입력 하세요.");
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 1또는 2를 입력 하세요.");
         }
     }
 }

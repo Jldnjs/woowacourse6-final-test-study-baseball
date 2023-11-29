be52f164a2c8d199bc6e3fbb57a78e060c40bcab
Jiwon <dtbsmf01@gmail.com>
2023-11-29 15:59:33 +0900

    feat: 사용자 기능 구현

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -1,2 +1,29 @@
-package baseball.domain;public class Player {
+package baseball.domain;
+
+import static baseball.domain.Validation.playerValidation;
+import static baseball.domain.Validation.restartNumValidation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Player {
+
+    public static List<Integer> playerNumber(String num){
+        List<Integer> playerNum = new ArrayList<>();
+        for (String s : num.split("")) {
+            playerNum.add(Integer.parseInt(s));
+        }
+        playerValidation(playerNum);
+        return playerNum;
+    }
+
+    public static boolean restartOrEnd(String num){
+        restartNumValidation(num);
+        if (num == "1") {
+            return true;
+        }
+        return false;
+    }
+
 }

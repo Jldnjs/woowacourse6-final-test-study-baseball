52576629d15e4c5d83af12a193bf599106245686
Jiwon <dtbsmf01@gmail.com>
2023-11-29 17:36:41 +0900

    style: 출력 구성 변경

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -5,6 +5,19 @@ public class OutputView {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
+    public static String printResult(int ball, int strike){
+        if (ball == 0 && strike == 0) {
+            return "낫싱";
+        }
+        if (strike == 0){
+            return ball+"볼";
+        }
+        if (ball == 0){
+            return strike + "스트라이크";
+        }
+        return ball + "볼 " + strike + "스트라이크";
+    }
+
     public static void printEnding(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }

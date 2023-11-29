ebf7f1cd39b09963ac7a29c329daa7d9f1b176e1
Jiwon <dtbsmf01@gmail.com>
2023-11-29 17:33:03 +0900

    style:

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -4,12 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public static String inputNumber(){
-        System.out.println("숫자를 입력해주세요 : ");
+    public static String inputNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
         return Console.readLine();
     }
 
-    public static String inputRestart(){
+    public static String inputRestart() {
         return Console.readLine();
     }
 

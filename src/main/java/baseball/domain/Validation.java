45cdf362637611e320f21bf7648c1c29d609e0fa
Jiwon <118653232+Jldnjs@users.noreply.github.com>
2023-11-30 16:36:50 +0900

    Update Validation.java

--- src/main/java/baseball/domain/Validation.java
+++ src/main/java/baseball/domain/Validation.java
@@ -13,12 +13,14 @@ public class Validation {
             throw new IllegalArgumentException("[ERROR] 3개의 숫자를 입력하세요.");
         }
     }
+    
     private static void duplicateValidation(List<Integer> playerNum){
         if (playerNum.size() != playerNum.stream().distinct().count()){
             throw new IllegalArgumentException("[ERROR] 중복된 요소가 있습니다.");
         }
     }
 
+    
     public static void restartNumValidation(String num) {
         if (!(num.equals("1") || num.equals("2"))) {
             throw new IllegalArgumentException("[ERROR] 1또는 2를 입력 하세요.");

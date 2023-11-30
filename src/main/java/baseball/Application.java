e52414ff4eebe52ca86e05e55fedb82d1f61ec2b
Jiwon <118653232+Jldnjs@users.noreply.github.com>
2023-11-30 16:37:21 +0900

    Update Application.java

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,5 +5,6 @@ import baseball.controller.BaseBallGameController;
 public class Application {
     public static void main(String[] args) {
         BaseBallGameController.run();
+        
     }
 }

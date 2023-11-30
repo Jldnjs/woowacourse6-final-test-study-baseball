bd7ecc9151b536bf50915fc6050921281800292f
Jiwon <118653232+Jldnjs@users.noreply.github.com>
2023-11-30 16:37:11 +0900

    Update OutputView.java

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -18,6 +18,7 @@ public class OutputView {
         return ball + "볼 " + strike + "스트라이크";
     }
 
+    
     public static void printEnding(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
